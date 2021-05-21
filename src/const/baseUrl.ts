239c1910fe26ea06a08587750946d9c0c6c5180d
Stanislav Karol <slsinitsin@gmail.com>
2021-05-21 12:37:46 +0300

    fix: определение BASE_URL

--- src/const/baseUrl.ts
+++ src/const/baseUrl.ts
@@ -1,4 +1,5 @@
 const production = "https://nfsw-telegram-bot.herokuapp.com";
 const development = "http://localhost:3000";
-const BASE_URL = process.env.NODE_ENV ? production : development;
+const BASE_URL =
+  process.env.NODE_ENV === "production" ? production : development;
 export default BASE_URL;
